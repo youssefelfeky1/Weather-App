23f228a8e668b63ad889cd142d58a3b4545eb0f3
Youssef_Elfeky <youssefelfeky83@gmail.com>
2025-05-04 20:14:18 +0300

    add: request permissions

--- app/src/main/java/com/elfeky/weather_app/presentation/MainActivity.kt
+++ app/src/main/java/com/elfeky/weather_app/presentation/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.elfeky.weather_app.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.elfeky.weather_app.presentation.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +26,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var allGranted by remember { mutableStateOf(false) }
+            val locationLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestMultiplePermissions(),
+                onResult = { isGranted ->
+                    allGranted = isGranted.all { it.value }
+                }
+            )
+            LaunchedEffect(key1 = locationLauncher) {
+                locationLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
             WeatherAppTheme {
-
+                if (allGranted) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Location Permissions Granted")
+                    }
+                } else {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            locationLauncher.launch(
+                                arrayOf(
+                                    Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION
+                                )
+                            )
+                        }) {
+                            Text("Request Location Permissions")
+                        }
+                    }
+                }
             }
         }
     }
